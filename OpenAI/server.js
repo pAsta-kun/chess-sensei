25d75480acc08fee20d116b09d36b2746c5e6013
pAsta-kun <ila.00123ivay@gmail.com>
2023-06-05 17:49:59 -0500

    idfk

--- OpenAI/server.js
+++ OpenAI/server.js
@@ -29,6 +29,7 @@ app.post('/', async (req, res) => {
   const response = await openAi.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
+      { role: "system", content: "You're the AI behind a chess website called Chess Sensei. Chess Sensei is an AI powered chess coach, analyzig users games and explaing why the best move is the best move. Some times you'll be provided with the users move, the best move, and the best move sequences after the best move has been played, other times you'll be given the entire game along with the mistakes made within the game. You may also be asked questions directly from the user."},
       { role: "user", content: requestData.message},
     ],
   })
