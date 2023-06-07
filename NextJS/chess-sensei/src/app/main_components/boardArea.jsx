487a0fdd6fe0a046b067578938059da433066474
pAsta-kun <ila.00123ivay@gmail.com>
2023-06-06 19:56:05 -0500

    now have access to PGN on page.jsx

--- NextJS/chess-sensei/src/app/main_components/boardArea.jsx
+++ NextJS/chess-sensei/src/app/main_components/boardArea.jsx
@@ -4,16 +4,9 @@ import Board from "./board"
 import InputBar from "./input"
 import { useState } from 'react';
 
-function DisplayBoard() 
+function DisplayBoard({ messages, i, onMessage }) 
 {
-    const [messages, setMessages] = useState([{message: "", display: true}]);
-    const [i, setI] = useState(0);
-    const handleMessage = (pgn, display) => {
-
-        setMessages(prevState => [...prevState, {pgn: pgn, display: display}]);
-        setI(i + 1);
-    };
-    
+   
     if(messages[i].display)
     {
         console.log(messages[i].pgn)
@@ -21,7 +14,7 @@ function DisplayBoard()
             <div className="w-2/5 mx-6 flex">
                 <Board/>
                 <div className="w-2/5 h-4/5 bg-background bg-opacity-70 rounded-2xl border-secondary border-4 flex justify-center items-center absolute z-10 p-5">
-                    <InputBar display={true} onMessage={handleMessage}/>
+                    <InputBar display={true} onMessage={onMessage}/>
                 </div>
             </div>
 
--- NextJS/chess-sensei/src/app/page.jsx
+++ NextJS/chess-sensei/src/app/page.jsx
@@ -1,14 +1,26 @@
+"use client";
+
 import Navbar from "./main_components/navbar"
 import Chatbox from "./main_components/chatbox"
 import Board from "./main_components/board"
 import DisplayBoard from "./main_components/boardArea"
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [messages, setMessages] = useState([{message: "", display: true}]);
+  const [i, setI] = useState(0);
+  const handleMessage = (pgn, display) => {
+
+      setMessages(prevState => [...prevState, {pgn: pgn, display: display}]);
+      setI(i + 1);
+  };
+
   return (
     <div className="h-full">
       <Navbar/>
       <div  className="flex justify-center h-full">
-        <DisplayBoard/>
+        <DisplayBoard messages={messages} i={i} onMessage={handleMessage}/>
         <Chatbox/>
       </div>
     </div>
